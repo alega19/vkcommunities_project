ccf9c80385ba7f83ecd608db258801af69b2bf99
alega19 <alega19@mail.ru>
2018-03-22 02:24:08 +1000

    created model VkAccount

--- datacollector/models.py
+++ datacollector/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+
+class VkAccount(models.Model):
+    phone = models.CharField(max_length=16, blank=True, null=True, unique=True)
+    email = models.EmailField(blank=True, null=True, unique=True)
+    password = models.TextField()
+    api_token = models.CharField(max_length=85, blank=True, null=True, unique=True)
+    enabled = models.BooleanField(default=True)
+    note = models.TextField(blank=True)
