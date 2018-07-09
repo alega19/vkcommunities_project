f10a46c52bc995d3f924d8a114f9847dcef6de57
alega19 <alega19@mail.ru>
2018-07-09 18:11:00 +1100

    add test config

---
+++ config/settings/test.py
@@ -0,0 +1,32 @@
+from .common import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)-8s %(name)s - %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'formatter': 'default',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'datacollector': {
+            'level': 'INFO',
+            'handlers': ['console'],
+        },
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
