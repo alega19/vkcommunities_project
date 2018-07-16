e3bfc39cd062f3652f17addb69b45cd8ce87e58b
alega19 <alega19@mail.ru>
2018-07-17 03:19:13 +1100

    fix POST requests to flower proxy
    Disabled CSRF checking and added another internal location for POST
    requests.

--- flower_proxy/views.py
+++ flower_proxy/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse, Http404
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 
+@require_http_methods(['GET', 'POST'])
+@csrf_exempt
 def index(req):
     user = req.user
-    if user.is_authenticated and user.is_staff:
-        resp = HttpResponse()
-        path = req.get_full_path()
-        new_path = path.replace('flower', 'internal-flower', 1)
-        resp['X-Accel-Redirect'] = new_path
-        return resp
-    raise Http404()
+    if not (user.is_authenticated and user.is_staff):
+        raise Http404()
+
+    if req.method == 'GET':
+        internal_location = 'internal-flower'
+    else:
+        internal_location = 'internal-flower-post'
+    resp = HttpResponse()
+    path = req.get_full_path()
+    new_path = path.replace('flower', internal_location, 1)
+    resp['X-Accel-Redirect'] = new_path
+    return resp
