27a63b179e42af68aad5ff64a70ffa52aea48c0a
alega19 <alega19@mail.ru>
2018-07-30 13:59:10 +1100

    refactor UserManager class

--- accounts/models.py
+++ accounts/models.py
@@ -8,15 +8,14 @@ from .tasks import send_mail
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password):
+        return self._create_user(email, password, is_staff=True, is_active=True)
+
+    def _create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
-        user = self.model(email=email, is_staff=True, is_active=True)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
