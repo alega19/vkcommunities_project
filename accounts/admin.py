e84f95839b84edd508a64a9386b6249a034f4036
alega19 <alega19@mail.ru>
2018-08-03 21:06:09 +1100

    fix adding/changing users in the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,45 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, Group
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import User
+from .forms import SignupForm
+
+
+class UserChangeForm(forms.ModelForm):
+
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'is_active', 'is_staff']
+
+    def clean_password(self):
+        return self.initial["password"]
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class MyUserAdmin(UserAdmin):
+
+    form = UserChangeForm
+    add_form = SignupForm
+
     list_display = ('email', 'is_staff', 'is_active', 'last_login', 'joined_at')
+    list_filter = ()
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'is_active', 'is_staff')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
+
     ordering = ('-joined_at',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
