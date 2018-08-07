bd28422e3c18428525ebf454a41809f28bdac1ca
alega19 <alega19@mail.ru>
2018-08-07 22:37:34 +1100

    add django-debug-toolbar for dev config

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,6 +5,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
+
+INTERNAL_IPS = ['127.0.0.1']
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
